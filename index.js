a133bd282965b257852e192d53b9bd9e298d6b8f
Maneth Dewpura <dewpuramaneth@gmail.com>
2025-08-28 17:32:56 +0530

    Remove unnecessary comments and whitespace from index.js for cleaner code

--- index.js
+++ index.js
@@ -3,68 +3,18 @@ import { PORT, MONGO_URI } from "./config.js";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from 'dotenv';
-//Maneth
 import itemsRoute from "./routes/itemsRoute.js";
 import cartRoute from "./routes/cartRoute.js";
 import cusItemsRoute from "./routes/cusItemsRoute.js";
-
-
-
-
-
-
-
-
-
-
-
-//Sandithi
 import reviewRoute from "./routes/reviewRoute.js";
-
-
-
-
-
-
-
-
-
-
-
-
-//Ridmi
 import deliveryInfoRoute from './routes/deliveryInfoRoute.js';
 import orderRoute from './routes/orderRoute.js';
 import paymentRoute from './routes/paymentRoute.js';
-
-
-
-
-
-
-
-
-
-
-
-//Hiranya
 import bodyMeasurementRoute from './routes/bodyMeasurementRoute.js';
 import usersRoute from './routes/usersRoute.js';
 import authRoute from './routes/auth.js';
 import empRoute from './routes/empRoute.js';
 import empAuthRoute from './routes/empAuth.js';
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const app = express();
@@ -79,70 +29,18 @@ app.get("/", (req, res) => {
 });
 
 
-
-//Maneth
 app.use("/items", itemsRoute);
 app.use("/cart", cartRoute);
 app.use("/cusItems", cusItemsRoute);
-
-
-
-
-
-
-
-
-
-
-
-//Sandithi
 app.use("/reviews", reviewRoute);
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Ridmi
 app.use("/deliveryInfo", deliveryInfoRoute);
 app.use("/orders", orderRoute);
 app.use("/payment", paymentRoute);
-
-
-
-
-
-
-
-
-
-
-//Hiranya
 app.use('/measurements', bodyMeasurementRoute);
 app.use('/users', usersRoute);
 app.use('/login', authRoute);
 app.use('/emps', empRoute);
 app.use('/empLogin', empAuthRoute);
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 mongoose
   .connect(MONGO_URI)
