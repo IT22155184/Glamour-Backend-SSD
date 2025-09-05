bebf497879220f80c93cb449863ff24c601082df
hiranya21 <128223556+IT22155184@users.noreply.github.com>
2025-09-05 14:27:09 +0530

    feat: add rate limiting middleware to enhance API security and prevent abuse

--- index.js
+++ index.js
@@ -44,6 +44,16 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 1000,
+  message: 'Too many requests from this IP, please try again later.',
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(limiter);
+
 app.use(sanitizeInput);
 
 app.use("/items", itemsRoute);
